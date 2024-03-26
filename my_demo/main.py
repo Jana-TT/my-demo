a9c0411f1d7e7f073f5a72d544baa7eed6269944
Jana-TT <janatt2056@gmail.com>
2024-03-25 21:33:38 -0500

    first commit

---
+++ my_demo/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"PP": "Poopie"}
+
+
+@app.get("/greeting/{greetings}")
+async def read_item(greetings: str):
+    return {"Wowie,": greetings}
